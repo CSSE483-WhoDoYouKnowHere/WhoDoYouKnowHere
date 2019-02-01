91aa02a813b90fe5bea831d7093326eb26f6b455
dripchar <dripchar@gmail.com>
2019-01-31 22:18:29 -0500

    Added EventOrgAdapter and EventOrgViewHolder.
    Added more functionality to the EventOrgActivity.
    
    Functionality close.

--- app/src/main/java/edu/rosehulman/whodoyouknowhere/whodoyouknowhere/EventOrgActivity.kt
+++ app/src/main/java/edu/rosehulman/whodoyouknowhere/whodoyouknowhere/EventOrgActivity.kt
@@ -10,36 +10,39 @@ import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.ToggleButton
+import com.google.type.Date
 import kotlinx.android.synthetic.main.activity_event_org.*
 import kotlinx.android.synthetic.main.add_event_dialog.view.*
 import kotlinx.android.synthetic.main.app_bar_event_org.*
+import java.util.*
 
 class EventOrgActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
-    lateinit var events: ArrayList<Event>
+    private lateinit var adapter: EventOrgAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_event_org)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            showAddDialog()
-        }
+
 
         val toggle = ActionBarDrawerToggle(
             this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
         )
+
+
+
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
 
         nav_view.setNavigationItemSelectedListener(this)
     }
 
-    fun buildEventCard() {
 
-    }
+    //Maybe imported the wrong view... (see "View?")
 
     override fun onBackPressed() {
         if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
@@ -63,39 +66,6 @@ class EventOrgActivity : AppCompatActivity(), NavigationView.OnNavigationItemSel
             R.id.action_settings -> return true
             else -> return super.onOptionsItemSelected(item)
         }
-    }
-
-    private fun showAddDialog() {
-        val builder = AlertDialog.Builder(this)
-        //Configure the builder: title, icon, message/custom view OR list.  Buttons (pos, neg, neutral)
-        builder.setTitle(getString(R.string.add_dialog_title))
-        val view = LayoutInflater.from(this).inflate(R.layout.add_event_dialog, null, false)
-        builder.setView(view)
-
-//        val toggleButton = view.findViewById<ToggleButton>(R.id.age_restriction_toggle)
-//        toggleButton?.setOnCheckedChangeListener { _, isChecked ->
-//            val msg = "Toggle Button is " + if (isChecked) "ON" else "OFF"
-//
-//        }
-
-
-        builder.setPositiveButton(android.R.string.ok) { _, _ ->
-
-            val eventName = view.add_name_edit_text.toString()
-            val eventDate = view.add_date_edit_text
-            val eventLocation = view.add_location_edit_text
-//            val isAgeRestriction = view.age_restriction_toggle.isChecked
-            val eventType = "Party"
-           // val entryFee = view.add_entry_fee_edit_text.text.to(CurrencyAmount)
-//            buildEvent(Event(eventName, ))
-
-        }
-        builder.setNegativeButton(android.R.string.cancel, null) // :)
-
-
-
-        builder.create().show()
-
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
